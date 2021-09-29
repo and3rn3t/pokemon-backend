528fead843f9201b9b50f8fb953ff43811a1ba96
Matthew Anderson <and3rn3t@icloud.com>
2021-09-29 10:20:40 -0500

    added foreign key to species

--- models/pokemonspecies.js
+++ models/pokemonspecies.js
@@ -5,11 +5,6 @@ const Pokemon = require("./pokemon");
 
 module.exports = (sequelize, DataTypes) => {
   class PokemonSpecies extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       PokemonSpecies.hasMany(models.Pokemon, {
         foreignKey: "speciesId",
