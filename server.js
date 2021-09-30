76570ded157b4dac34be9b8f398738eb661ccabc
Matthew Anderson <and3rn3t@icloud.com>
2021-09-30 14:12:56 -0500

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // MAIN
 app.get("/", (req, res) => {
